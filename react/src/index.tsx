4a4872159077d4c8836e296ca837abba55e69bfc
Ask Morken <ask.morken@nexigroup.com>
2024-06-03 22:08:31 +0200

    react/build

--- react/src/index.tsx
+++ react/src/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import App from './App';
+import App from './App';
 import { initializeApp } from 'firebase/app';
-import Home from './LoggedIn/Home';
 
 const firebaseConfig = require('./firebase-config.json').result.sdkConfig;
 initializeApp(firebaseConfig);
@@ -11,5 +10,5 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <Home />
+  <App />
 );
