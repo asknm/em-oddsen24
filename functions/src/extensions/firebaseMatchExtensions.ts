91d870ddd97a8274b64ea145695b99f57293a0d0
Ask Morken <ask.morken@nexigroup.com>
2024-06-05 17:45:51 +0200

    fixing fetchAllMatchesFromApi

--- functions/src/extensions/firebaseMatchExtensions.ts
+++ functions/src/extensions/firebaseMatchExtensions.ts
@@ -1,12 +1,11 @@
-// import { Timestamp } from "firebase-admin/firestore";
-// import { ApiMatch } from "common";
-// import { FirebaseMatch } from "../constants";
-// import { fromApiTeam } from "./teamExtensions";
+import { Timestamp } from "firebase-admin/firestore";
+import { fromApiTeam } from "./teamExtensions";
+import { ApiMatch, FirebaseMatch } from "../domain/match";
 
-// export function fromApiMatch(apiMatch: ApiMatch): FirebaseMatch {
-//     return {
-//         utcDate: Timestamp.fromMillis(Date.parse(apiMatch.utcDate)),
-//         homeTeam: fromApiTeam(apiMatch.homeTeam),
-//         awayTeam: fromApiTeam(apiMatch.awayTeam),
-//     };
-// }
+export function fromApiMatch(apiMatch: ApiMatch): FirebaseMatch {
+    return {
+        utcDate: Timestamp.fromMillis(Date.parse(apiMatch.utcDate)),
+        homeTeam: fromApiTeam(apiMatch.homeTeam),
+        awayTeam: fromApiTeam(apiMatch.awayTeam),
+    };
+}
