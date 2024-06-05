af7edf4775f141951f3de8358c4babf12f1b00f5
Ask Morken <ask.morken@nexigroup.com>
2024-06-05 18:45:02 +0200

    try fix

--- functions/src/handlers/fetchMatchesFromApi.ts
+++ functions/src/handlers/fetchMatchesFromApi.ts
@@ -5,19 +5,17 @@ import { ApiMatch, FirebaseMatch } from "../domain/match";
 import { fromApiMatch } from "../extensions/firebaseMatchExtensions";
 
 export async function fetchAllMatchesFromApiHandler(db: Firestore, apiKey: string) {
-    try {
-        const matchesFromApi = await getMatchesFromApi(apiKey);
-        await Promise.all(matchesFromApi.map(apiMatch => storeMatchInDb(apiMatch)));
+    const matchesFromApi = await getMatchesFromApi(apiKey);
+    await Promise.all(matchesFromApi.map(apiMatch => storeMatchInDb(apiMatch)));
 
-        async function storeMatchInDb(apiMatch: ApiMatch) {
-            const matchRef = matchDoc(db, apiMatch.id.toString());
-            const snapshot = await matchRef.get();
-            const matchData = snapshot.data();
-            if (matchData) {
-                return;
-            }
-            const firebaseMatch: FirebaseMatch = fromApiMatch(apiMatch);
-            await matchRef.set(firebaseMatch);
+    async function storeMatchInDb(apiMatch: ApiMatch) {
+        const matchRef = matchDoc(db, apiMatch.id.toString());
+        const snapshot = await matchRef.get();
+        const matchData = snapshot.data();
+        if (matchData) {
+            return;
         }
-    } 
+        const firebaseMatch: FirebaseMatch = fromApiMatch(apiMatch);
+        await matchRef.set(firebaseMatch);
+    }
 }
