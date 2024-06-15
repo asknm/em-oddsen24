08c3348185f94383c86c35d7ceabe54f34b89bbf
Ask Morken <ask.morken@nexigroup.com>
2024-06-15 13:40:41 +0200

    no transaction

--- functions/src/handlers/settleDebts.ts
+++ functions/src/handlers/settleDebts.ts
@@ -1,4 +1,4 @@
-import { DocumentReference, Firestore } from "firebase-admin/firestore";
+import { DocumentReference, FieldValue, Firestore } from "firebase-admin/firestore";
 import { FirebaseMatch } from "../domain/match";
 import { correctOddsOption, oddsValue } from "../extensions/oddsExtensions";
 import { betCollection } from "../extensions/betExtensions";
@@ -18,38 +18,28 @@ export async function settleDebts(db: Firestore, matchDayId: string, matchId: st
     const oddsV = oddsValue(odds, correctOption);
     const betCollectionRef = betCollection(db, matchDayId, matchId);
 
-    await db.runTransaction(async (t) => {
-        const snapshot = await t.get(betCollectionRef);
-        for (const betDoc of snapshot.docs) {
-            const bet = betDoc.data();
-            if (bet.selection === correctOption) {
-                const winAmount = bet.amount * (oddsV - 1);
-                await transfer(odds.bookmaker.id, betDoc.id, winAmount);
-            }
-            else {
-                await transfer(betDoc.id, odds.bookmaker.id, bet.amount);
-            }
+    const snapshot = await betCollectionRef.get();
+    for (const betDoc of snapshot.docs) {
+        const bet = betDoc.data();
+        if (bet.selection === correctOption) {
+            const winAmount = bet.amount * (oddsV - 1);
+            await transfer(odds.bookmaker.id, betDoc.id, winAmount);
         }
-
-        async function transfer(from: string, to: string, amount: number) {
-            const fromRef = userDoc(db, from);
-            const ToRef = userDoc(db, to);
-            await incrementBalance(fromRef, -amount);
-            await incrementBalance(ToRef, amount);
-
-            async function incrementBalance(userDoc: DocumentReference<UserWithBalance>, amount: number) {
-                const snapshot = await t.get(userDoc)
-                const user = snapshot.data();
-                if (!user) {
-                    throw new Error(`Could not find user with id ${userDoc.id}`);
-                }
-                const oldBalance = user?.balance ?? 0;
-                const newBalance = oldBalance + amount;
-                logger.info('Incrementing balance of ', user.name, ' from ', oldBalance, ' by ', amount, ' to ', newBalance);
-                t.update(userDoc, {
-                    "balance": newBalance,
-                });
-            }
+        else {
+            await transfer(betDoc.id, odds.bookmaker.id, bet.amount);
         }
-    });
+    }
+
+    async function transfer(from: string, to: string, amount: number) {
+        const fromRef = userDoc(db, from);
+        const ToRef = userDoc(db, to);
+        await incrementBalance(fromRef, -amount);
+        await incrementBalance(ToRef, amount);
+
+        async function incrementBalance(userDoc: DocumentReference<UserWithBalance>, amount: number) {
+            return userDoc.update({
+                "balance": FieldValue.increment(amount),
+            });
+        }
+    }
 }
